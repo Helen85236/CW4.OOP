571b6dee50ff6fc9facb5037cb8a235b5a4b7741
Helen85236 <79532870251@yandex.ru>
2023-06-05 21:32:37 +0300

    create

---
+++ dir/supjob_api.py
@@ -0,0 +1,42 @@
+"""Работа класса, для работы с ресурсом SupJob.ru"""
+import os
+import requests
+from dir.working_with_API import Working
+from dir.vacancies import Vacancy
+
+
+class SJAPI(Working):
+    def __init__(self):
+        self.api_key = os.getenv('API_KEY')
+        self.headers = {'X-Api-App-ID': self.api_key}
+        self.url = 'https://api.superjob.ru/2.0/vacancies/'
+
+    def get_vacancies(self, search_query: str):
+        """Получение вакансий при помощи SJAPI"""
+        params = {'keyword': search_query, 'count': 100}
+        response = requests.get(self.url, headers=self.headers, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            vacancies_data = data['objects']
+            vacancies = []
+            for vacancy in vacancies_data:
+                title = vacancy['profession']
+                salary = SJAPI.get_salary(vacancy)
+                description = vacancy['candidat']
+                url = vacancy['link']
+                vacancy = Vacancy(title, salary, description, url)
+                vacancies.append(vacancy)
+            return vacancies
+        else:
+            return f'Error: {response.status_code}'
+
+    @staticmethod
+    def get_salary(vacancy, **kwargs):
+        if vacancy.get('payment_to') == 0:
+            salary = {'from': vacancy['payment_from'], 'currency': vacancy['currency']}
+        elif vacancy.get('payment_from') == 0:
+            salary = {'from': vacancy['payment_to'], 'currency': vacancy['currency']}
+        else:
+            salary = {'from': vacancy.get('payment_from', 0), 'to': vacancy.get('payment_to', 0),
+                      'currency': vacancy.get('currency', 'rub')}
+        return salary
---
+++ main.py
@@ -0,0 +1,4 @@
+from dir.utils import user_interaction
+
+if __name__ == "__main__":
+    user_interaction()
